e103191fbf51a09748268a19d2e27da30ab05c87
shoya6666 <shoya061666@icloud.com>
2025-05-09 14:42:01 +0900

    tese

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int Recursive1(int kyuuryou, int zikan, int kotei) {
+	if (kyuuryou >= kotei) {
+		printf("%dŠÔŒã‚É’´‚¦‚é", zikan);
+		return zikan;
+	}
+	printf("%d@%d\n", kyuuryou, kotei);
+	kyuuryou += kyuuryou *2 - 50;
+
+	return (Recursive1(kyuuryou, ++zikan,kotei += 1072));
+}
+
+int main() {
+
+	int saiki = 100;
+	int ippan = 1072;
+	int zikan = 1;
+	int resullt1 = Recursive1(saiki, zikan, ippan);;
+
+	return 0;
+}
+
